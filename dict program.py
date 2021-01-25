a56c1adb3b3b3e31f71fb5c19240b598028ed03a
Megh Patel <meghpatel945@gmail.com>
2021-01-25 11:42:37 +0530

    init

---
+++ dict program.py
@@ -0,0 +1,31 @@
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+
+print ="dict['Name']: " 
+
+print ="dict['Age']:"
+
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+
+print ="dict['Alice']: "
+
+dict = {'Name':'Zara','Age': 7 ,'Class':'First'}
+
+dict = {'Age': 8 };
+
+dict = {'School': 'DPS School'};
+
+print = "dict[Age]"
+
+print = "dict[School]"
+
+dict = {'Name': 'Zara','Age': 7,'class':'First'}
+del dict['Name'];
+dict.clear();
+del dict ;
+
+print = "dict[Age]:"
+print = "dict[School]:"
+
+dict = {'Name':'Zara','Age': 7,'Name':'Mehul'}
+print = "dict['Name]";
+
