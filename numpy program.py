fdba77421de562ebb025d1c81791ea14a3c65e8a
megh7002 <77950499+megh7002@users.noreply.github.com>
2021-01-28 17:42:50 +0530

    Add files via upload

---
+++ numpy program.py
@@ -0,0 +1,44 @@
+import numpy as np
+a=np.array([1,2,3])
+print = (a)
+
+a=np.array([(1,2,3),(4,5,6)])
+print = (a)
+
+import numpy as np
+ 
+import time
+import sys
+S= range(1000)
+print(sys.getsizeof(5)*len(S))
+ 
+D= np.arange(1000)
+print = (D.size*D.itemsize)
+
+import time
+import sys
+ 
+SIZE = 1000000
+ 
+L1= range(SIZE)
+L2= range(SIZE)
+A1= np.arange(SIZE)
+A2=np.arange(SIZE)
+ 
+start= time.time()
+result=[(x,y) for x,y in zip(L1,L2)]
+print((time.time()-start)*1000)
+ 
+start=time.time()
+result= A1+A2
+print = ((time.time()-start)*1000)
+
+import numpy as np
+a = np.array([(8,9,10),(11,12,13)])
+print(a)
+a=a.reshape(3,2)
+print = (a)
+
+import numpy as np
+a=np.array([(1,2,3,4),(3,4,5,6)])
+print = (a[0:,2])
